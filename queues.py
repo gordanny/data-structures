4a8722d6b00ce34a96f42e0892205cf95c73be21
Denis Gorshkov <gordanny.social@gmail.com>
2021-07-04 02:33:38 +0500

    Added implementing a queue using a deque from collections

--- queues.py
+++ queues.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 class ListQueue:
     def __init__(self):
         self.queue = []
@@ -10,3 +13,17 @@ class ListQueue:
 
     def pop(self):
         return self.queue.pop(0)
+
+
+class DequeQueue:
+    def __init__(self):
+        self.queue = deque()
+
+    def is_empty(self):
+        return not len(self.queue)
+
+    def push(self, item):
+        self.queue.append(item)
+
+    def pop(self):
+        return self.queue.popleft()
