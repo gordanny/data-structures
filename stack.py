be9586242cede559ec088bda191e9ca27f19b07a
Denis Gorshkov <gordanny.social@gmail.com>
2021-07-03 22:15:22 +0500

    Added implementing a stack using a deque from collections

--- stack.py
+++ stack.py
@@ -1,6 +1,26 @@
+from collections import deque
+
+
 class ListStack:
     def __init__(self):
         self.stack = []
+
+    def is_empty(self):
+        return not len(self.stack)
+
+    def push(self, element):
+        self.stack.append(element)
+
+    def pop(self):
+        return self.stack.pop()
+
+    def top(self):
+        return self.stack[-1]
+
+
+class DequeStack:
+    def __init__(self):
+        self.stack = deque()
 
     def is_empty(self):
         return not len(self.stack)
